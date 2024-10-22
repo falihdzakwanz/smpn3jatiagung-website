0a81a871f5b890d2b955e6edee50f6162412e8b2
Dito Rifki Irawan <dito.122140153@student.itera.ac.id>
2024-10-22 22:52:10 +0700

    Revert "Merge pull request #1 from falihdzakwanz/BE-Login-Dito"
    This reverts commit 214a94b724075ea83c2b31b655e8b6db4057c5f7, reversing
    changes made to 873e9d52234d7af5773565b6b3e661bff9aff7a5.

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -15,7 +15,7 @@ export default function Login({
     canResetPassword: boolean;
 }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        username: '',
+        email: '',
         password: '',
         remember: false,
     });
@@ -40,20 +40,20 @@ export default function Login({
 
             <form onSubmit={submit}>
                 <div>
-                    <InputLabel htmlFor="username" value="Username" />
+                    <InputLabel htmlFor="email" value="Email" />
 
                     <TextInput
-                        id="username"
-                        type="text"
-                        name="username"
-                        value={data.username}
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={data.email}
                         className="mt-1 block w-full"
                         autoComplete="username"
                         isFocused={true}
-                        onChange={(e) => setData('username', e.target.value)}
+                        onChange={(e) => setData('email', e.target.value)}
                     />
 
-                    <InputError message={errors.username} className="mt-2" />
+                    <InputError message={errors.email} className="mt-2" />
                 </div>
 
                 <div className="mt-4">
