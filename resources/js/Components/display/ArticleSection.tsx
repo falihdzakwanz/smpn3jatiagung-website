d6c63f3cec5d14764d8be6940c972918b749181a
Falih <falihdzakwan028@gmail.com>
2024-11-05 17:18:11 +0700

    refactor: news detail page

--- resources/js/Components/display/ArticleSection.tsx
+++ resources/js/Components/display/ArticleSection.tsx
@@ -2,10 +2,10 @@ const ArticleSection = (props: any) => {
     const { title, text } = props; 
     return (
             <div className="mt-8 flex w-full flex-col items-center justify-center gap-4">
-                <h1 className="text-2xl font-bold md:text-4xl">
+                <h1 className="text-2xl font-bold md:text-4xl capitalize text-color-primary">
                     {title}
                 </h1>
-                <p className="whitespace-pre-wrap text-justify">
+                <p className="whitespace-pre-wrap text-justify text-color-primary">
                     {text}
                 </p>
             </div>
