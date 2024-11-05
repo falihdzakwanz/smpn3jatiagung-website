d7adfe6efcb0d65859ef2de4a361a6255fc21ba4
Falih <falihdzakwan028@gmail.com>
2024-11-05 17:04:40 +0700

    refactor: news list page

--- resources/js/Components/news/Card.tsx
+++ resources/js/Components/news/Card.tsx
@@ -1,23 +1,38 @@
 import { Link } from "@inertiajs/react";
 
 const Card = (props: any) => {
-    const { id, title, body } = props;
+    const { id, title, body, imageSrc } = props;
+
+    const truncateAfterFourDots = (text: string) => {
+        let dotCount = 0;
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] === '.') {
+                dotCount++;
+                if (dotCount === 4) {
+                    return text.substring(0, i + 1);
+                }
+            }
+        }
+        return text;
+    };
 
     return (
-        <div className="flex flex-col items-center justify-center px-4 gap-4 md:flex-row">
-            <div className="flex items-center md:w-1/4">
+        <div className="flex flex-col items-center justify-center p-4 gap-4 md:flex-row border-b-2 border-b-color-primary">
+            <div className="flex items-center md:w-1/4 rounded-xl overflow-hidden">
                 <img
-                    src="https://www.dummyimage.com/450x300/000/fff&text=LOGO"
-                    alt="Logo"
-                    className="object-cover max-w-full max-h-60"
+                    src={imageSrc}
+                    alt={`Berita - ${id}`}
+                    className="object-cover max-w-full max-h-48"
                 />
             </div>
             <div>
-                <h2 className="mb-4 font-bold uppercase md:text-2xl">
+                <h2 className="mb-4 font-bold uppercase md:text-2xl text-color-primary">
                     {title}
                 </h2>
-                <p className="mb-2 text-justify md:text-base">{body}</p>
-                <Link href={route('news.show', id)} className="font-bold">
+                <p className="mb-2 text-justify text-sm md:text-base text-color-primary">
+                    {truncateAfterFourDots(body)}
+                </p>
+                <Link href={route('news.show', id)} className="font-bold text-color-primary text-sm md:text-base">
                     Read More <span>&#187;</span>
                 </Link>
             </div>
--- resources/js/Pages/News.tsx
+++ resources/js/Pages/News.tsx
@@ -1,39 +1,32 @@
-import Footer from '@/Components/fragments/Footer';
-import Navbar from '@/Components/fragments/Navbar';
 import Card from '@/Components/news/Card';
+import GuestLayout from '@/Layouts/GuestLayout';
 import { Head } from '@inertiajs/react';
 
 const News = () => {
     return (
-        <>
+        <GuestLayout>
             <Head title="News" />
-            <header>
-                <Navbar />
-            </header>
-
-            <main>
-                <div className="flex flex-col items-center justify-center gap-8 p-8">
-                    <Card
-                        id="1"
-                        title="Lorem ipsum, dolor sit amet"
-                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dolore consequuntur accusantium quisquam veritatis quis totam molestiae optio iste architecto."
-                    />
-                    <div className="w-full border-2 border-gray-950"></div>
-                    <Card
-                        id="2"
-                        title="Lorem ipsum, dolor sit amet"
-                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dolore consequuntur accusantium quisquam veritatis quis totam molestiae optio iste architecto."
-                    />
-                    <div className="w-full border-2 border-gray-950"></div>
-                    <Card
-                        id="3"
-                        title="Lorem ipsum, dolor sit amet"
-                        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dolore consequuntur accusantium quisquam veritatis quis totam molestiae optio iste architecto."
-                    />
-                </div>
-            </main>
-            <Footer />
-        </>
+            <div className="flex flex-col items-center justify-center gap-8 bg-color-secondary px-8 py-6">
+                <Card
+                    id="1"
+                    title="Lorem ipsum, dolor sit amet"
+                    body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dolore consequuntur accusantium quisquam veritatis quis totam molestiae optio iste architecto. Jarip. Jarip. Jarip"
+                    imageSrc="https://www.dummyimage.com/450x300/000/fff&text=LOGO"
+                />
+                <Card
+                    id="2"
+                    title="Lorem ipsum, dolor sit amet"
+                    body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dolore consequuntur accusantium quisquam veritatis quis totam molestiae optio iste architecto."
+                    imageSrc="https://www.dummyimage.com/450x300/000/fff&text=LOGO"
+                />
+                <Card
+                    id="3"
+                    title="Lorem ipsum, dolor sit amet"
+                    body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus dolore consequuntur accusantium quisquam veritatis quis totam molestiae optio iste architecto."
+                    imageSrc="https://www.dummyimage.com/450x300/000/fff&text=LOGO"
+                />
+            </div>
+        </GuestLayout>
     );
 };
 
