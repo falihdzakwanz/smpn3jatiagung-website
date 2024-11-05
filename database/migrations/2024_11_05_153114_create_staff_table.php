7595317b65b947e7ba2b59aad99c5d06aa87cbdc
himuraaaaa <xzanovpro14@gmail.com>
2024-11-06 03:11:47 +0700

    PrestasiCOntroller, Factory, Model

--- database/migrations/2024_11_05_153114_create_staff_table.php
+++ database/migrations/2024_11_05_153114_create_staff_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('prestasi', function (Blueprint $table) {
+        Schema::create('staff', function (Blueprint $table) {
             $table->id();
             $table->string('judul');
             $table->string('foto')->nullable();
