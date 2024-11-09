298cdcaecdd1e89d0cf880451566f8fca999db1e
Falih <falihdzakwan028@gmail.com>
2024-11-09 20:29:33 +0700

    feat: anchor to home

--- resources/js/Components/fragments/Navbar/NavbarLogo.tsx
+++ resources/js/Components/fragments/Navbar/NavbarLogo.tsx
@@ -1,18 +1,40 @@
+import { Link } from '@inertiajs/react';
+
 interface NavbarLogoProps {
     text: string;
+    isHomePage: boolean;
     className?: string;
 }
 
-const NavbarLogo = ({ text, className = '' }: NavbarLogoProps) => {
-    return (
-        <a href="/" className={`flex items-center gap-3 ${className}`}>
+const NavbarLogo = (props: NavbarLogoProps) => {
+    const { text, isHomePage, className } = props;
+
+    const Logo = () => (
+        <>
             <img
                 src="https://salman-gebaeudereinigung.de/wp-content/uploads/2022/10/logo-salman.png"
                 alt="Logo Sekolah"
                 className="h-12 w-auto object-contain"
             />
-            <span className="font-bold text-2xl">{text}</span>
-        </a>
+            <span className="text-xl md:text-2xl font-bold tracking-wider">{text}</span>
+        </>
+    );
+
+    return (
+        <>
+            {isHomePage ? (
+                <a href="/" className={`flex items-center gap-3 ${className}`}>
+                    <Logo />
+                </a>
+            ) : (
+                <Link
+                    href="/"
+                    className={`flex items-center gap-3 ${className}`}
+                >
+                    <Logo />
+                </Link>
+            )}
+        </>
     );
 };
 
