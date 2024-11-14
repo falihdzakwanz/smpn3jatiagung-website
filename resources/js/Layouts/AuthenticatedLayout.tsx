70fe88f75bb1b6c6028e0e8289f77f836ecedc28
Falih <falihdzakwan028@gmail.com>
2024-11-14 22:57:22 +0700

    feat: admin layout with navbar

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,15 +1,23 @@
+import AuthenticatedNavbar from '@/Components/fragments/Navbar/AuthenticatedNavbar';
 import Sidebar from '@/Components/fragments/Sidebar';
 import { usePage } from '@inertiajs/react';
 import { PropsWithChildren, useState } from 'react';
 
 const AuthenticatedLayout = ({children}: PropsWithChildren) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const user = usePage().props.auth.user;
-
+const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+};
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="bg-color-secondary` min-h-screen">
             <Sidebar isOpen={isSidebarOpen} />
-            <main className={`${isSidebarOpen && "ml-96"} px-8 py-4`}>{children}</main>
+            <AuthenticatedNavbar
+                toggleSidebar={toggleSidebar}
+                isOpen={isSidebarOpen}
+            />
+            <main className={`${isSidebarOpen && 'ml-96'} px-8 py-4 duration-300 transition-all`}>
+                {children}
+            </main>
         </div>
     );
 }
